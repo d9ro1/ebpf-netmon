49a104429f697eb258206b254335b2a54fcb37df
y.arrar <y.arrar@outlook.com>
2026-09-22 05:09:03 +0200

    fix: include bpf_core_read.h and bpf_endian.h for BPF_CORE_READ/bpf_ntohs

--- bpf/tcp_probes.c
+++ bpf/tcp_probes.c
@@ -3,6 +3,8 @@
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
+#include <bpf/bpf_core_read.h>
+#include <bpf/bpf_endian.h>
 
 char __license[] SEC("license") = "Dual MIT/GPL";
 
